fbddcc1d4e8c6c18ed73b247002e57a2260de00b
shsv382 <shsv382@gmail.com>
2019-02-08 21:46:59 +0300

    Updates

---
+++ precache-manifest.0984e8a9fd47a9871160268dfd8fcae3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd99404e7a195940cec7",
+    "url": "/meeterest/static/js/runtime~main.bd99404e.js"
+  },
+  {
+    "revision": "79ec89fab59c9c4e69f1",
+    "url": "/meeterest/static/js/main.79ec89fa.chunk.js"
+  },
+  {
+    "revision": "fc341ce1abf09dc58554",
+    "url": "/meeterest/static/js/1.fc341ce1.chunk.js"
+  },
+  {
+    "revision": "79ec89fab59c9c4e69f1",
+    "url": "/meeterest/static/css/main.4314901d.chunk.css"
+  },
+  {
+    "revision": "1b68a03eaf30061716ed199042a76a8e",
+    "url": "/meeterest/index.html"
+  }
+];
